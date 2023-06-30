8b10449aeaa17c6961d5a786bd74d83f5e39e8f0
Keks <keks@htmlacademy.ru>
2023-06-30 16:43:51 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/pictures.js
+++ 7/js/pictures.js
@@ -5,12 +5,12 @@ export const createPictures = (pictureElements) => {
 
   const picturesListFragment = document.createDocumentFragment();
 
-  pictureElements.forEach((element) => {
+  pictureElements.forEach(({url, description, likes, comments}) => {
     const pictureElement = pictureElementTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = element.url;
-    pictureElement.querySelector('.picture__img').alt = element.description;
-    pictureElement.querySelector('.picture__info').querySelector('.picture__comments').textContent = element.likes;
-    pictureElement.querySelector('.picture__info').querySelector('.picture__likes').textContent = element.comments.length;
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__img').alt = description;
+    pictureElement.querySelector('.picture__info').querySelector('.picture__comments').textContent = likes;
+    pictureElement.querySelector('.picture__info').querySelector('.picture__likes').textContent = comments.length;
     picturesListFragment.appendChild(pictureElement);
   });
 
